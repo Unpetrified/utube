2d786038f2f12b0031bff13cd3af9139c2f1640c
Unpetrified <kingsleyobiefuna01@gmail.com>
2023-01-31 02:00:17 +0100

    updated

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('txt');
 const form = document.getElementById('form');
 
-btn.addEventListener('click', () => {
-  navigator.clipboard.writeText(text.innerHTML);
-  alert('Link copied');
-  window.open('https://tuberipper.com/');
-})
-
 form.addEventListener('submit', (e) => {
+  
   e.preventDefault();
   let old = form['original'].value;
+  try {
+  
   var code = old.split('.com/live/')[1].split('?')[0];
   
   var newLink = 'https://youtu.be/'+code;
   
-  text.innerHTML = newLink;
-  
+  navigator.clipboard.writeText(newLink);
+  alert('Link copied');
+  window.open('https://tuberipper.com/');
+
+  } catch (e) {
+    alert('Check the link to make sure it is correct')
+  }
 });
